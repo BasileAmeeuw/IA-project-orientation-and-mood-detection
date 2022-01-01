33c7393c74ca1aeaf5006ce80cd9a09fc1299e19
AMEEUW Basile <basile.ameeuw@mil.be>
2022-01-01 19:10:34 +0100

    projet IA

--- emotion_training.py
+++ emotion_training.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import utils
 import os
-
+import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Dense, Input, Dropout,Flatten, Conv2D
 from tensorflow.keras.layers import BatchNormalization, Activation, MaxPooling2D
@@ -15,10 +15,25 @@ from tensorflow.keras.utils import plot_model
 from livelossplot import PlotLossesKeras
 
 import tensorflow as tf
+import argparse
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", type=str,
+	default="dataset_emotion/",
+	help="path to input dataset")
+ap.add_argument("-p", "--plot", type=str, default="plot_emotion.png",
+	help="path to output loss/accuracy plot")
+ap.add_argument("-m", "--model", type=str,
+	default="models/model.json",
+	help="path to output face mask detector model")
+args = vars(ap.parse_args())
+
 print("Tensorflow version:", tf.__version__)
 gpus = tf.config.experimental.list_physical_devices('GPU') 
 tf.config.experimental.set_memory_growth(gpus[0], True)
-datasetPath="dataset_emotion/"
+datasetPath=args["dataset"]
 for expression in os.listdir(datasetPath+"train/"):
     print(str(len(os.listdir(datasetPath+"train/"+expression))) + " "+expression + " images")
 img_size = 224
@@ -90,7 +105,7 @@ checkpoint = ModelCheckpoint("model_weights.h5", monitor='val_accuracy',
                             save_weights_only=True, mode='max', verbose=1)
 reduce_lr= ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, min_lr=0.00001, mode='auto')
 callbacks = [checkpoint, reduce_lr]
-history = model.fit(
+H = model.fit(
     train_generator,
     steps_per_epoch=steps_per_epoch,
     epochs=epochs,
@@ -104,7 +119,18 @@ history = model.fit(
 #     json_file.write(model_json)
 
 model_json = model.to_json()
-with open("model.json","w") as json_file:
+with open(args["model"],"w") as json_file:
     json_file.write(model_json)
-
     
+N = epochs
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
